2ed97c5772f728efb6f6ed4eaad437c522f71eb3
Daneshwaranm <danesh@appinessworld.com>
2020-06-05 11:30:14 +0530

    Layout, toast and google play service.json error fixed

--- app/src/main/java/com/example/firebasecrud/MainActivity.java
+++ app/src/main/java/com/example/firebasecrud/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.firebasecrud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+        private EditText text;
+        private Button button;
+        FirebaseDatabase rootNode;
+        DatabaseReference reference;
+
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (EditText) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "This is a message displayed in a Toast",
+                                Toast.LENGTH_SHORT);
+
+                        toast.show();
+                }
+        });
+
         }
         }
